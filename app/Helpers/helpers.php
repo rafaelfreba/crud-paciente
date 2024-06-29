fd3f21156d4e6d8a2ad87e9b4dd111f883d95083
Rafael <rafaelfreba@gmail.com>
2024-06-29 12:16:50 -0400

    Fix: corrigindo erro na digitação

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -3,7 +3,7 @@
 if (!function_exists('getFoto')) {
     function getFoto($foto = null)
     {
-        $url = $foto ? (public_path('storage') . '/' . $foto) : (public_path('images') . '/' . mt_rand(1,9) . '.png'); 
+        $url = $foto ?  (public_path('storage') . '/' . $foto) : (public_path('images') . '/' . mt_rand(1,9) . '.png'); 
         return base64_encode(file_get_contents($url));
     }
 }
